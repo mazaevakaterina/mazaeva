59cdf27d1215c079055dadc4838de603045d5909
itisioslab <itisioslab@Mac-mini-administrator-2.local>
2014-07-01 10:39:09 +0400

    Initial Commit

---
+++ Ka/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Ka
+//
+//  Created by itisioslab on 01.07.14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
